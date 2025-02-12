af6dad815e8a6188f33f385da37b9198ba0110ec
Egoriy <bulkin.egor2002@mail.ru>
2025-02-13 00:26:25 +0400

    working add goals but with all values

--- frontend/src/api/goals.js
+++ frontend/src/api/goals.js
@@ -4,3 +4,8 @@ export const getGoals = async () => {
   const response = await api.get('/goals/');
   return response.data;
 };
+
+export const createGoal = async (goalData) => {
+    const response = await api.post('/goals/', goalData);
+    return response.data;
+};
